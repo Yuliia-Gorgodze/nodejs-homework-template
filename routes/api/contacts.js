7125efe2611bd6e4764ad5f8a378a59f926772eb
Yuliia-Gorgodze <taragorgodze@gmail.com>
2021-05-28 00:04:45 +0300

    add route

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,59 @@
 const express = require('express')
 const router = express.Router()
+const Contacts = require('../../model')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Contacts.listContacts()
+    return res.json({ status: 'succes', code: 200, data: { result } })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Contacts.getContactById(req.params.contactId)
+    if (result) {
+      return res.json({ status: 'succes', code: 200, data: { result } })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Contacts.addContact(req.body)
+    res.status(201).json({ status: 'succes', code: 201, data: { result } })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Contacts.removeContact(req.params.contactId)
+    if (result) {
+      return res.json({ status: 'succes', code: 200, data: { result } })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
 })
 
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await Contacts.updateContact(req.params.contactId, req.body)
+    if (result) {
+      return res.json({ status: 'succes', code: 200, data: { result } })
+    }
+    return res.json({ status: 'error', code: 404, message: 'Not found' })
+  } catch (e) {
+    next(e)
+  }
 })
 
 module.exports = router
